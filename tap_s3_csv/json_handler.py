def74ec15ff2099b169eca0d31b29af43f1d9079
Yashrb21 <y.r.bagayatkar@gmail.com>
2022-10-05 15:04:55 -0400

    Add files via upload

---
+++ tap_s3_csv/json_handler.py
@@ -0,0 +1,9 @@
+import codecs
+from io import TextIOWrapper
+import json
+def get_row_iterator(table_spec,file_handle):
+    file_stream = codecs.iterdecode(file_handle._raw_stream, encoding='utf-8')
+    for line in file_handle._raw_stream.readlines():
+        json_line = json.loads(line)
+        keys = list(json_line['subject'].keys())
+        yield json_line['subject'][keys[0]]
