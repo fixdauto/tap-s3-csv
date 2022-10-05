cd745dc68a88de18674efd57650a90efccdc1ef5
Yashrb21 <y.r.bagayatkar@gmail.com>
2022-10-05 15:54:46 -0400

    Update json_handler.py

--- tap_s3_csv/json_handler.py
+++ tap_s3_csv/json_handler.py
@@ -4,6 +4,7 @@ import json
 def get_row_iterator(table_spec,file_handle):
     file_stream = codecs.iterdecode(file_handle._raw_stream, encoding='utf-8')
     for line in file_handle._raw_stream.readlines():
+        json_line = json.loads(line)
         outer_keys = json_line.keys()
         if 'subject' in outer_keys:
             keys = list(json_line['subject'].keys())
